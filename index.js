c9ea19271b3046142e616bca121fa2fdb0f3eaa0
Rimantas <belauzas@yahoo.com>
2022-08-02 10:54:54 +0300

    Solution

--- index.js
+++ index.js
@@ -1,46 +1,61 @@
 import { Cirkas } from "./js/Cirkas.js";
+console.clear();
 
-const besieliuCikras = new Cirkas('Besieliai');
+const besieliuCirkas = new Cirkas('Besieliai');
 
-besieliuCikras.intro();
+console.log(besieliuCirkas.intro());
 // Sveiki atvyke i "Besieliai" cirka!
 
-besieliuCikras.tvarkarastis();
+console.log('\n' + besieliuCirkas.tvarkarastis());
 // "Besieliai" cirko tvarkarastis:
 // Siuo metu pasirodymu nera suplanuota.
 
-besieliuCikras.naujasPasirodymas(2022, 9, 1);
+console.log('\n' + besieliuCirkas.naujasPasirodymas(2022, 9, 1));
 // Naujas pasirodymas 2022 Rugsėjo 1 dieną!
 
-besieliuCikras.naujasPasirodymas(2022, 8, 25);
+console.log('\n' + besieliuCirkas.naujasPasirodymas(2022, 8, 25));
 // Naujas pasirodymas 2022 Rugpjūčio 25 dieną!
 
-besieliuCikras.naujasPasirodymas(2022, 3, 17);
+console.log('\n' + besieliuCirkas.naujasPasirodymas(2022, 3, 17));
 // Naujas pasirodymas 2022 Kovo 17 dieną!
 
 // READ: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
-besieliuCikras.tvarkarastis();
+console.log('\n' + besieliuCirkas.tvarkarastis());
 // "Besieliai" cirko tvarkarastis:
 // 1) 2022 Kovo 17d (jau įvykęs);
 // 2) 2022 Rugpjūčio 25d;
 // 3) 2022 Rugsėjo 1d.
 
-besieliuCikras.pakeistiData(3, 2022, 9, 2);
+console.log('\n' + besieliuCirkas.pakeistiData(3, 2022, 9, 2));
 // Pasirodymo data pakeista į 2022 Rugsėjo 2 dieną!
 
-besieliuCikras.tvarkarastis();
+console.log('\n' + besieliuCirkas.tvarkarastis());
 // "Besieliai" cirko tvarkarastis:
 // 1) 2022 Kovo 17d (jau įvykęs);
 // 2) 2022 Rugpjūčio 25d;
 // 3) 2022 Rugsėjo 2d.
 
-besieliuCikras.atsauktiPasirodyma(1);
+console.log('\n' + besieliuCirkas.atsauktiPasirodyma(1));
 // Atsaukti jau praejusio pasirodymo negalima!
 
-besieliuCikras.atsauktiPasirodyma(2);
+console.log('\n' + besieliuCirkas.atsauktiPasirodyma(2));
 // Pasirodymas atsauktas (2022 Rugpjūčio 25d)!
 
-besieliuCikras.tvarkarastis();
+console.log('\n' + besieliuCirkas.tvarkarastis());
 // "Besieliai" cirko tvarkarastis:
 // 1) 2022 Kovo 17d (jau įvykęs);
 // 2) 2022 Rugsėjo 2d.
+
+console.log('\n' + besieliuCirkas.atsauktiPasirodyma(2));
+// Pasirodymas atsauktas (2022 Rugsejo 2d)!
+
+console.log('\n' + besieliuCirkas.tvarkarastis());
+// "Besieliai" cirko tvarkarastis:
+// 1) 2022 Kovo 17d (jau įvykęs).
+
+console.log('\n' + besieliuCirkas.atsauktiPasirodyma(1));
+// Atsaukti jau praejusio pasirodymo negalima!
+
+console.log('\n' + besieliuCirkas.tvarkarastis());
+// "Besieliai" cirko tvarkarastis:
+// 1) 2022 Kovo 17d (jau įvykęs).
--- js/Cirkas.js
+++ js/Cirkas.js
@@ -1,5 +1,56 @@
 class Cirkas {
+    constructor(name) {
+        this.name = name;
+        this.list = [];
+        this.months = ['Sausio', 'Vasario', 'Kovo', 'Balandzio',
+            'Geguzes', 'Birzelio', 'Liepos', 'Rugpjucio',
+            'Rugsejo', 'Spalio', 'Lapkricio', 'Gruodzio'];
+    }
 
+    intro() {
+        return `Sveiki atvyke i "${this.name}" cirka!`;
+    }
+
+    tvarkarastis() {
+        let res = `"Besieliai" cirko tvarkarastis:`;
+
+        if (this.list.length === 0) {
+            res += `\nSiuo metu pasirodymu nera suplanuota.`;
+        } else {
+            let index = 0;
+            for (const item of this.list) {
+                res += `\n${++index}) ${item.y} ${this.months[item.m - 1]} ${item.d}d`;
+                if (item.mili < Date.now()) {
+                    res += ` (jau įvykęs)`;
+                }
+                res += index < this.list.length ? ';' : '.';
+            }
+        }
+
+        return res;
+    }
+
+    naujasPasirodymas(y, m, d) {
+        this.list.push({ y, m, d, mili: new Date(y, m - 1, d).getTime() });
+        this.list.sort((a, b) => a.mili - b.mili);
+        return `Naujas pasirodymas ${y} ${this.months[m - 1]} ${d} dieną!`
+    }
+
+    pakeistiData(nr, y, m, d) {
+        this.list[nr - 1] = { y, m, d, mili: new Date(y, m - 1, d).getTime() };
+        this.list.sort((a, b) => a.mili - b.mili);
+        return `Pasirodymo data pakeista į ${y} ${this.months[m - 1]} ${d} dieną!`
+    }
+
+    atsauktiPasirodyma(nr) {
+        if (this.list[nr - 1].mili < Date.now()) {
+            return `Atsaukti jau praejusio pasirodymo negalima!`;
+        } else {
+            const item = this.list[nr - 1];
+            this.list.splice(nr - 1, 1);
+            return `Pasirodymas atsauktas (${item.y} ${this.months[item.m - 1]} ${item.d}d)!`;
+        }
+    }
 }
 
 export { Cirkas }
